fdb125b2c2bf4310c2c945e94719d89e6ab458c5
SaiMadhavSunkara <sunkarasaimadhav@gmail.com>
2026-04-03 15:40:05 +0530

    Add: two-integer-sum - submission-10

---
+++ Data Structures & Algorithms/two-integer-sum/submission-10.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash = {}
+            
+        for i in range(len(nums)):
+            temp = target - nums[i]
+            if temp in hash and i != hash[temp]:
+                return [hash[temp], i]
+            hash[nums[i]] = i
+
+        return []
